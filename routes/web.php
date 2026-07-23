eddcc816d31bb96363089709a2cfeaf1630d0f91
yusuf <yurtlama@gmail.com>
2026-07-23 11:48:02 +0300

    test: verify automated Coolify redeploy on git push

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
+// Route definition for Adisyon Application - Auto Deploy Test
 Route::get('/', function () {
     return redirect()->route('dashboard');
 });
