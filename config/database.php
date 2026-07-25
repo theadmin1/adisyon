52ab4cb076c09756c881270de0e2ff50bf7dea70
Velat Aydemir <info@altf4soft.com>
2026-07-25 14:18:55 +0300

    fix(config): set default DB_HOST to 172.17.0.1 for containerized MySQL access

--- config/database.php
+++ config/database.php
@@ -47,7 +47,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '172.17.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'adisyon'),
             'username' => env('DB_USERNAME', 'adisyon'),
